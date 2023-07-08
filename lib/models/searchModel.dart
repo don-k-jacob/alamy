d873c2beb0d5fd540aeaf1b6a9ef12c7c4ad2d2a
don-k-jacob <donkjacob.business@gmail.com>
2023-07-09 04:37:12 +0530

    added lotti

---
+++ lib/models/searchModel.dart
@@ -0,0 +1,243 @@
+// To parse this JSON data, do
+//
+//     final propertyOwnerPropertiesModel = propertyOwnerPropertiesModelFromJson(jsonString);
+
+import 'dart:convert';
+
+ImagesModel imagesModelFromJson(String str) =>
+    ImagesModel.fromJson(json.decode(str));
+
+String imagesModelToJson(ImagesModel data) => json.encode(data.toJson());
+
+class ImagesModel {
+  final Images images;
+
+  ImagesModel({
+    required this.images,
+  });
+
+  factory ImagesModel.fromJson(Map<String, dynamic> json) => ImagesModel(
+        images: Images.fromJson(json["IMAGES"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "IMAGES": images.toJson(),
+      };
+}
+
+class Images {
+  final List<I> i;
+  final List<ResultElement> results;
+
+  Images({
+    required this.i,
+    required this.results,
+  });
+
+  factory Images.fromJson(Map<String, dynamic> json) => Images(
+        i: List<I>.from(json["I"].map((x) => I.fromJson(x))),
+        results: List<ResultElement>.from(
+            json["RESULTS"].map((x) => ResultElement.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "I": List<dynamic>.from(i.map((x) => x.toJson())),
+        "RESULTS": List<dynamic>.from(results.map((x) => x.toJson())),
+      };
+}
+
+class I {
+  final IClass empty;
+
+  I({
+    required this.empty,
+  });
+
+  factory I.fromJson(Map<String, dynamic> json) => I(
+        empty: IClass.fromJson(json["\u0024"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "\u0024": empty.toJson(),
+      };
+}
+
+class IClass {
+  final String id;
+  final String ar;
+  final String li;
+  final String mr;
+  final String pr;
+  final String r;
+  final String pixX;
+  final String pixY;
+  final String type;
+  final String premium;
+  final String caption;
+  final String lc;
+  final String datetaken;
+  final String p;
+  final String t;
+  final String flp;
+  final String dataco;
+  final String imgseq;
+  final String pseudoid;
+  final DateTime uploaddate;
+  final String seocontribpriority;
+
+  IClass({
+    required this.id,
+    required this.ar,
+    required this.li,
+    required this.mr,
+    required this.pr,
+    required this.r,
+    required this.pixX,
+    required this.pixY,
+    required this.type,
+    required this.premium,
+    required this.caption,
+    required this.lc,
+    required this.datetaken,
+    required this.p,
+    required this.t,
+    required this.flp,
+    required this.dataco,
+    required this.imgseq,
+    required this.pseudoid,
+    required this.uploaddate,
+    required this.seocontribpriority,
+  });
+
+  factory IClass.fromJson(Map<String, dynamic> json) => IClass(
+        id: json["ID"],
+        ar: json["AR"],
+        li: json["LI"],
+        mr: json["MR"],
+        pr: json["PR"],
+        r: json["R"],
+        pixX: json["PIX_X"],
+        pixY: json["PIX_Y"],
+        type: json["TYPE"],
+        premium: json["PREMIUM"],
+        caption: json["CAPTION"],
+        lc: json["LC"],
+        datetaken: json["DATETAKEN"],
+        p: json["P"],
+        t: json["T"],
+        flp: json["FLP"],
+        dataco: json["DATACO"],
+        imgseq: json["IMGSEQ"],
+        pseudoid: json["pseudoid"],
+        uploaddate: DateTime.parse(json["uploaddate"]),
+        seocontribpriority: json["seocontribpriority"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "ID": id,
+        "AR": ar,
+        "LI": li,
+        "MR": mr,
+        "PR": pr,
+        "R": r,
+        "PIX_X": pixX,
+        "PIX_Y": pixY,
+        "TYPE": type,
+        "PREMIUM": premium,
+        "CAPTION": caption,
+        "LC": lc,
+        "DATETAKEN": datetaken,
+        "P": p,
+        "T": t,
+        "FLP": flp,
+        "DATACO": dataco,
+        "IMGSEQ": imgseq,
+        "pseudoid": pseudoid,
+        "uploaddate": uploaddate.toIso8601String(),
+        "seocontribpriority": seocontribpriority,
+      };
+}
+
+class ResultElement {
+  final Result empty;
+
+  ResultElement({
+    required this.empty,
+  });
+
+  factory ResultElement.fromJson(Map<String, dynamic> json) => ResultElement(
+        empty: Result.fromJson(json["\u0024"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "\u0024": empty.toJson(),
+      };
+}
+
+class Result {
+  final String images;
+  final String agencies;
+  final String searchTypeSi;
+  final String hardClass;
+  final String collections;
+  final String cds;
+  final String bestof;
+  final String commcount;
+  final String custcount;
+  final String totalagencyimages;
+  final String totalphotographerimages;
+  final String agencyimages;
+  final String agency;
+  final String st;
+
+  Result({
+    required this.images,
+    required this.agencies,
+    required this.searchTypeSi,
+    required this.hardClass,
+    required this.collections,
+    required this.cds,
+    required this.bestof,
+    required this.commcount,
+    required this.custcount,
+    required this.totalagencyimages,
+    required this.totalphotographerimages,
+    required this.agencyimages,
+    required this.agency,
+    required this.st,
+  });
+
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+        images: json["IMAGES"],
+        agencies: json["AGENCIES"],
+        searchTypeSi: json["SEARCH_TYPE_SI"],
+        hardClass: json["HARD_CLASS"],
+        collections: json["COLLECTIONS"],
+        cds: json["CDS"],
+        bestof: json["BESTOF"],
+        commcount: json["COMMCOUNT"],
+        custcount: json["CUSTCOUNT"],
+        totalagencyimages: json["TOTALAGENCYIMAGES"],
+        totalphotographerimages: json["TOTALPHOTOGRAPHERIMAGES"],
+        agencyimages: json["AGENCYIMAGES"],
+        agency: json["AGENCY"],
+        st: json["ST"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "IMAGES": images,
+        "AGENCIES": agencies,
+        "SEARCH_TYPE_SI": searchTypeSi,
+        "HARD_CLASS": hardClass,
+        "COLLECTIONS": collections,
+        "CDS": cds,
+        "BESTOF": bestof,
+        "COMMCOUNT": commcount,
+        "CUSTCOUNT": custcount,
+        "TOTALAGENCYIMAGES": totalagencyimages,
+        "TOTALPHOTOGRAPHERIMAGES": totalphotographerimages,
+        "AGENCYIMAGES": agencyimages,
+        "AGENCY": agency,
+        "ST": st,
+      };
+}
---
+++ lib/services/getImages.dart
@@ -0,0 +1,25 @@
+import 'package:alamy/models/searchModel.dart';
+import 'package:http/http.dart' as http;
+
+Future<ImagesModel> fetchData() async {
+  var url = Uri.parse('https://silly-teal-cockatoo.cyclic.app/get-all');
+
+  var response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    // Request successful
+    var responseBody = response.body;
+    print(responseBody);
+    return imagesModelFromJson(responseBody);
+  } else {
+    // Request failed
+    print('Error: ${response.statusCode}');
+    return ImagesModel(
+      images: Images(
+        i: [],
+        results: [],
+      ),
+    );
+  }
+}
+
